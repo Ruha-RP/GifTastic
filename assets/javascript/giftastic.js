7fda0f4bf80a94e4bd943be09fe8b5fe64ca75f6
Ruha Peera <ruha.rizwan@gmail.com>
2017-12-17 13:45:02 -0500

    connected to API

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -6,29 +6,35 @@ $("#addAnimal").on("click", function(event) {
 	//Ensuring that the page doesn't submit itself
 	event.preventDefault();
 
-	
+//STEP 1: Making the submit button extract information from the Giphy API	
 
 
-});
+	//Grabbing the user's input text, and putting the information in a new varibale called animal
+	var animal = $('#animal-input').val();
 
-//closing the on-click function
+	//Creating the query URL, which is one of the parameters of the AJAX call. Limited to 10 results
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=AkFwbCSaIKXeFXXJc1BOIfByIy9ai35t&limit=10"
 
+	//the AJAX call
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
 
-//Creating the query URL
-var queryURL = "";
-//the AJAX call
-$.ajax({
-	url: queryURL,
-	method: "GET"
-}).done(function(response) {
 	//checking the response
 	console.log(response);
 
-});
+	//displaying the JSON in the animal buttons div as a string
+	$("#animalButtons").text(JSON.stringify(response));
+
+
+	
+
+	});//closing the AJAX call
 
 
 
-
+});//closing the on-click function
 
 
 
