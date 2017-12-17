7360ae764210d6f03dfcec62278f0706b117159d
Ruha Peera <ruha.rizwan@gmail.com>
2017-12-17 00:42:30 -0500

    partial pseudo-coded JS file

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
 
+//Creation of an on-click event for the submit button, this will lead to the AJAX call being executed
+$("#addAnimal").on("click", function(event) {
 
+	//Ensuring that the page doesn't submit itself
+	event.preventDefault();
+
+	
+
+
+});
+
+//closing the on-click function
+
+
+//Creating the query URL
+var queryURL = "";
+//the AJAX call
+$.ajax({
+	url: queryURL,
+	method: "GET"
+}).done(function(response) {
+	//checking the response
+	console.log(response);
+
+});
 
 
 
